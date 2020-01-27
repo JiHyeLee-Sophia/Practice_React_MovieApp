9d82aa0687723c1b08d4b2f6d0a43ca6cbe5c017
JihyeLee_SOPHIA <sophapia@users.noreply.github.com>
2020-01-27 15:53:48 +0900

    Updates

---
+++ precache-manifest.824f3bc0cda40af9be957a2ba66f05cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5f0588f3d52456b4dbe7883cbaf868",
+    "url": "/practice_react_movieapp/index.html"
+  },
+  {
+    "revision": "061d1c3eb4ad9f2ee6a4",
+    "url": "/practice_react_movieapp/static/css/main.ca5ed02a.chunk.css"
+  },
+  {
+    "revision": "ff59bf264731b8e8101f",
+    "url": "/practice_react_movieapp/static/js/2.f02dc836.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/practice_react_movieapp/static/js/2.f02dc836.chunk.js.LICENSE"
+  },
+  {
+    "revision": "061d1c3eb4ad9f2ee6a4",
+    "url": "/practice_react_movieapp/static/js/main.f9809beb.chunk.js"
+  },
+  {
+    "revision": "462796d62a1a4d9a8b9b",
+    "url": "/practice_react_movieapp/static/js/runtime-main.0cd8de3d.js"
+  }
+]);
